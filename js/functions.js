e66d417bf48f72ac691e6d2defef79705f6e453e
Mandragora <sara.cueva.rodriguez@hotmail.com>
2020-04-28 16:32:31 +0200

    subido a gitHub

---
+++ js/functions.js
@@ -0,0 +1,32 @@
+
+// $$ ACORDEON
+const acordeon = document.querySelectorAll('[class*="acordeon"]');
+
+if (acordeon != null) {
+   acordeon.forEach(function (item) {
+      let alturaElemento = item.nextElementSibling.scrollHeight;
+      item.nextElementSibling.style = `height: ${alturaElemento}px`;
+
+      item.addEventListener('click', function () {
+         let alturaActual = this.nextElementSibling.offsetHeight;
+
+         (alturaActual > 0) ? (
+            this.nextElementSibling.style = 'height: 0',
+            this.style.setProperty('--rotation', 'rotate(0deg)')
+         ) : (
+               this.nextElementSibling.style = `height: ${alturaElemento}px;`,
+               this.style.setProperty('--rotation', 'rotate(180deg)'))
+      })
+   })
+}
+
+// $$ APERTURA MENU
+
+
+// $$ SCROLL EN X
+var item = document.getElementsByTagName('DIV')[1];
+
+window.addEventListener('wheel', function (e) {
+   if (e.deltaY > 0) item.scrollLeft += 100;
+   else item.scrollLeft -= 100;
+});
